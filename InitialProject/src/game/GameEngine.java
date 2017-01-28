4becfe6bf8f4938e4686ff72b4dcc541fe3f524c
Venelin Bakalov <venelin_bakalov@abv.bg>
2017-01-29 01:34:39 +0200

    Created Player.class, InputHandler.class and some other small stuff. Check workflow.docx

---
+++ InitialProject/src/game/GameEngine.java
@@ -0,0 +1,129 @@
+package game;
+
+import display.Display;
+import gfx.Assets;
+import gfx.SpriteSheet;
+import states.GameState;
+import states.MenuState;
+import states.StateManager;
+
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class GameEngine implements Runnable {
+    private String title;
+
+    private Display display;
+    private BufferStrategy bs;
+    private Graphics g;
+
+    private Thread thread;
+    private boolean isRunning;
+
+    private GameState gameState;
+    private MenuState menuState;
+
+    private InputHandler inputHandler;
+
+
+    public GameEngine (String name) {
+        this.title = name;
+    }
+
+    private void init() {
+
+        this.display = new Display(this.title);
+
+        gameState = new GameState();
+        menuState = new MenuState();
+
+        this.inputHandler = new InputHandler(this.display);
+        StateManager.setCurrentState(gameState);
+       // this.sh = new SpriteSheet(Assets.player, 95, 130);
+      //  SpriteSheet background = new SpriteSheet(Assets.background, 800, 600);
+    }
+
+    private void tick() {
+        if (StateManager.getCurrentState() != null) {
+            StateManager.getCurrentState().tick();
+        }
+    }
+
+    private void render() {
+        this.bs = this.display.getCanvas().getBufferStrategy();
+
+        if (this.bs == null) {
+            this.display.getCanvas().createBufferStrategy(2);
+            return;
+        }
+
+        this.g = this.bs.getDrawGraphics();
+
+        g.clearRect(0, 0, Display.WIDTH, Display.HEIGHT);
+
+        //Start drawing
+
+        if (StateManager.getCurrentState() != null) {
+            StateManager.getCurrentState().render(g);
+        }
+
+        //g.drawImage(this.sh.crop(0, i), 100, 100, null);
+
+
+        //END drawing
+        this.g.dispose();
+        this.bs.show();
+    }
+
+    @Override
+    public void run() {
+        this.init();
+
+        int fps = 30;
+        double timePerTick = 1_000_000_000.0 / fps;
+        double delta = 0;
+        //The current time in nanoseconds
+        long now;
+        long lastTime = System.nanoTime();
+
+        while (isRunning) {
+            //Sets the variable to the current time in nanoseconds
+            now = System.nanoTime();
+
+            delta += (now - lastTime) / timePerTick;
+            lastTime = now;
+
+            if (delta >= 1) {
+                //If we don't want to lower the framerate take the this.tick() outside the while loop.
+                this.tick();
+                this.render();
+                delta--;
+
+            }
+
+
+        }
+
+        this.stop();
+    }
+
+    public synchronized void start() {
+        if (!isRunning) {
+            this.thread = new Thread(this);
+
+            this.isRunning = true;
+            this.thread.start();
+        }
+    }
+
+    public synchronized void stop() {
+        if (isRunning) {
+            try {
+                this.isRunning = false;
+                this.thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- InitialProject/src/states/GameState.java
+++ InitialProject/src/states/GameState.java
@@ -1,4 +1,34 @@
 package states;
 
-public class GameState extends State{
+import display.Display;
+import game.entities.Player;
+import gfx.Assets;
+
+import java.awt.*;
+
+public class GameState extends State {
+    private static final int GRAVITY = 2;
+    public static Player player;
+
+    public GameState() {
+        init();
+
+        player = new Player("Nakovkata", 95, 130, 100, 400);
+    }
+
+    private void init() {
+        Assets.init();
+    }
+
+
+    @Override
+    public void tick() {
+        player.tick();
+    }
+
+    @Override
+    public void render(Graphics g) {
+        player.render(g);
+
+    }
 }
